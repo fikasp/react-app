250d6d91fca1d1f2aa8aedcb4582140358b5872d
Przemysław Fikas <fikasp@gmail.com>
2022-10-12 21:56:46 +0200

    Modify Copyright

--- src/components/copyright/Copyright.js
+++ src/components/copyright/Copyright.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Copyright.css';
+import styles from './styles.module.css'
+import './styles.scss';
 
 class Copyright extends React.Component {
     render() {
         return (
             <div className="copy">
-                <span >Copyright &copy; {this.props.year} example.com - All rights reserved
+                <span className={styles.info}>Copyright &copy; {this.props.year} example.com - All rights reserved
                 </span>
             </div>
         )
